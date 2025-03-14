9fdc2f7c2e6351e33534d948984c2e92a45993f8
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2025-03-14 21:28:36 +0530

    add env variables

--- Program.cs
+++ Program.cs
@@ -2,9 +2,27 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Outgo_tracker_Backend.Data;
+
 using System.Text.Json.Serialization; // Import required namespace
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+  // Load environment variables from .env file
+  DotNetEnv.Env.Load();
+}
+
+
+// Add environment variables to configuration
+builder.Configuration["ConnectionStrings:DefaultConnection"] = Environment.GetEnvironmentVariable("DefaultConnection");
+builder.Configuration["AzureAdB2C:Instance"] = Environment.GetEnvironmentVariable("AzureAdB2C__Instance");
+builder.Configuration["AzureAdB2C:ClientId"] = Environment.GetEnvironmentVariable("AzureAdB2C__ClientId");
+builder.Configuration["AzureAdB2C:Domain"] = Environment.GetEnvironmentVariable("AzureAdB2C__Domain");
+builder.Configuration["AzureAdB2C:TenantId"] = Environment.GetEnvironmentVariable("AzureAdB2C__TenantId");
+builder.Configuration["AzureAdB2C:SignUpSignInPolicyId"] = Environment.GetEnvironmentVariable("AzureAdB2C__SignUpSignInPolicyId");
+builder.Configuration["AzureAdB2C:ResetPasswordPolicyId"] = Environment.GetEnvironmentVariable("AzureAdB2C__ResetPasswordPolicyId");
+builder.Configuration["AzureAdB2C:Audience"] = Environment.GetEnvironmentVariable("AzureAdB2C__Audience");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
